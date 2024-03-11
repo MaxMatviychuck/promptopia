22d401adb21bab5cf810efea1fcd06633eba0306
Maksym Matviichuck <maksymmatviichuck@LGEs-MacBook-Pro.local>
2024-03-11 14:05:16 +0200

    init

---
+++ components/PromptCard.jsx
@@ -0,0 +1,98 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const [copied, setCopied] = useState("");
+
+  const handleProfileClick = () => {
+    console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
+  const handleCopy = (event) => {
+    event.stopPropagation();
+    setCopied(post.prompt);
+
+    navigator.clipboard.writeText(post.prompt);
+
+    setTimeout(() => {
+      setCopied("");
+    }, 1500);
+  };
+
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-card gap-5">
+        <div
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
+          <Image
+            src={post?.creator?.image}
+            alt=""
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-500">
+              {post?.creator?.username}
+            </h3>
+            <p className="font-inter text-sm ext-gray-500">
+              {post?.creator?.email}
+            </p>
+          </div>
+          <button className="copy_btn" onClick={handleCopy}>
+            <Image
+              src={
+                copied === post.prompt
+                  ? "/assets/icons/tick.svg"
+                  : "assets/icons/copy.svg"
+              }
+              alt=""
+              width={12}
+              height={12}
+            />
+          </button>
+        </div>
+      </div>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="font-inner text-sm blue-gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
+
+      {session?.user.id === post.creator._id && pathname === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font_inter text-sm green_gradient cursor-pointer"
+            onClick={() => handleEdit(post)}
+          >
+            Edit
+          </p>
+          <p
+            className="font_inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PromptCard;
---
+++ utils/database.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("db is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+    });
+
+    isConnected = true;
+    console.log("db is connected");
+  } catch (error) {
+    console.log("db connection error", error);
+  }
+};
